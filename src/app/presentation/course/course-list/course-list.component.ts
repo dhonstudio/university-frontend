5b989804ce94f5e45d15c09c477cc2f33ea43800
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-09 09:26:36 +0700

    add course repository

--- src/app/presentation/course/course-list/course-list.component.ts
+++ src/app/presentation/course/course-list/course-list.component.ts
@@ -1,25 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
-export interface Course {
-  id: number,
-  title: string;
-  credit: number;
-}
-
-const ELEMENT_DATA: Course[] = [
-  {id: 1, title: 'Hydrogen', credit: 1.0079},
-  {id: 2, title: 'Helium', credit: 4.0026},
-  {id: 3, title: 'Lithium', credit: 6.941},
-  {id: 4, title: 'Beryllium', credit: 9.0122},
-  {id: 5, title: 'Boron', credit: 10.811},
-  {id: 6, title: 'Carbon', credit: 12.0107},
-  {id: 7, title: 'Nitrogen', credit: 14.0067},
-  {id: 8, title: 'Oxygen', credit: 15.9994},
-  {id: 9, title: 'Fluorine', credit: 18.9984},
-  {id: 10, title: 'Neon', credit: 20.1797},
-];
-
 @Component({
   selector: 'app-course-list',
   standalone: true,
