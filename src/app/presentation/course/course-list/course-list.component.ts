773bb1cfe851c8d463eb9d309569db5016163282
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-09 10:53:42 +0700

    add dialog

--- src/app/presentation/course/course-list/course-list.component.ts
+++ src/app/presentation/course/course-list/course-list.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { CourseFeature } from '../../../application/features/course.feature';
 import { Course } from '../../../domain/entities/course.entity';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatButtonModule, MatDialogModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
@@ -14,7 +17,10 @@ export class CourseListComponent implements OnInit {
   displayedColumns: string[] = ['courseID', 'title', 'credits'];
   dataSource: Course[] = [];
 
-  constructor(private _courseFeature: CourseFeature) { }
+  constructor(
+    private _courseFeature: CourseFeature,
+    private _matDialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this._courseFeature.getCourseList().subscribe(result => {
@@ -22,5 +28,7 @@ export class CourseListComponent implements OnInit {
     })
   }
 
-  
+  onAddButtonClik() {
+    this._matDialog.open(CourseDialogComponent);
+  }
 }
