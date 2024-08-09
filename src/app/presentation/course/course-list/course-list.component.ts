d0983d22b1f3702ad8a8500c4cf3826727e1d419
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-09 09:13:38 +0700

    update model to course

--- src/app/presentation/course/course-list/course-list.component.ts
+++ src/app/presentation/course/course-list/course-list.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface Course {
+  id: number,
+  title: string;
+  credit: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: Course[] = [
+  {id: 1, title: 'Hydrogen', credit: 1.0079},
+  {id: 2, title: 'Helium', credit: 4.0026},
+  {id: 3, title: 'Lithium', credit: 6.941},
+  {id: 4, title: 'Beryllium', credit: 9.0122},
+  {id: 5, title: 'Boron', credit: 10.811},
+  {id: 6, title: 'Carbon', credit: 12.0107},
+  {id: 7, title: 'Nitrogen', credit: 14.0067},
+  {id: 8, title: 'Oxygen', credit: 15.9994},
+  {id: 9, title: 'Fluorine', credit: 18.9984},
+  {id: 10, title: 'Neon', credit: 20.1797},
 ];
 
 @Component({
@@ -29,6 +28,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrl: './course-list.component.scss'
 })
 export class CourseListComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'title', 'credit'];
   dataSource = ELEMENT_DATA;
 }
