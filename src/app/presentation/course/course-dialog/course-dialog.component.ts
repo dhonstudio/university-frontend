0ca4c817907cd72e23385735b500028fc1227496
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-09 11:05:35 +0700

    add dialog content

--- src/app/presentation/course/course-dialog/course-dialog.component.ts
+++ src/app/presentation/course/course-dialog/course-dialog.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Course } from '../../../domain/entities/course.entity';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, FormsModule],
   templateUrl: './course-dialog.component.html',
   styleUrl: './course-dialog.component.scss'
 })
 export class CourseDialogComponent {
+  public course: Course = {
+    courseID: 0,
+    title: '',
+    credits: 0
+  };
 
+  onSaveButtonClick() {
+
+  }
 }
