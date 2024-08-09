4659c3ffa9c34d332d9afdfb0fd9d22b337b9561
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-09 08:49:43 +0700

    add course-list routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'university-frontend';
+  title = 'University App';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CourseListComponent } from './presentation/course/course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CourseListComponent
+    },
+    {
+        path: 'course',
+        component: CourseListComponent
+    }
+];
